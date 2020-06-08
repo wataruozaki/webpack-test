3cd8d24495798e9da71e1078a1c76f819478d7b1
wataru <dossjakapoko@gmail.com>
2020-06-08 22:36:16 +0900

    startupSet

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/javascripts/main.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "javascripts/main.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: "css-loader"
+          }
+        ]
+      },
+      {
+        test: /\.pug/,
+        use: [
+          {
+            loader: "html-loader"
+          },
+          {
+            loader: "pug-html-loader",
+            options: {
+              pretty: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./stylesheets/main.css"
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/templates/index.pug",
+      filename: "index.html"
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
